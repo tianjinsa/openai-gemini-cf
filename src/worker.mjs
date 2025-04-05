8c3771c4f9b55f85bc6b0f6509521b439dbf3ac3
tianjinsa <107697707+tianjinsa@users.noreply.github.com>
2025-04-05 21:38:11 +0800

    Update worker.mjs

--- src/worker.mjs
+++ src/worker.mjs
@@ -1,7 +1,7 @@
 import { Buffer } from "node:buffer";
 
 // 默认的Google API密钥
-const DEFAULT_API_KEY = "";
+const DEFAULT_API_KEY = "AIzaSyAahN4VChovJV49XEhf9FXvrIuhV0BEJwQ";
 
 export default {
   async fetch (request) {
